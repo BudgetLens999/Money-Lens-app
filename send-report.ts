1c33e535737b1c4969f57ca4bf9575fd4e22e288
BudgetLens999 <jamiesonjackson52@gmail.com>
2026-06-17 14:31:56 -0400

    Delete send-report.ts

--- send-report.ts
+++
@@ -1,9 +0,0 @@
-import EmailReportModal from '../components/EmailReportModal';
-
-<EmailReportModal
-  month="June 2026"
-  totalSpent={totalSpent}
-  totalBudget={totalBudget}
-  categories={categoryArray}   // [{ name, spent, budget }]
-  userName={user?.name}        // optional
-/>
